0edb550851ff996459e06a0f94efba7b40d838b9
Ravikant Munda <126383121+RavikantMunda@users.noreply.github.com>
2025-05-29 23:10:11 +0530

    Customer Auth

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,6 +2,7 @@
 
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null, // The authenticated user object
   isCheckingAuth: true, // Indicates if the authentication status is being checked
@@ -18,6 +19,44 @@ export const useAuthStore = create((set) => ({
       set({ authUser: null }); //Explicitly set authUser to null if there's an error
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully!");
+    } catch (error) {
+      console.error("Error signing up:", error);
+      toast.error(error.response?.data?.message || "Signup failed");
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully!");
+    } catch (error) {
+      console.error("Error logging in:", error);
+      toast.error(error.response?.data?.message || "Login failed");
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/signout");
+      set({ authUser: null });
+      toast.success("Logged out successfully!");
+    } catch (error) {
+      console.error("Error logging out:", error);
+      toast.error(error.response?.data?.message || "Logout failed");
     }
   },
 }));
